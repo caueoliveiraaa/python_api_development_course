b365ed537f1176b3152686996010e6dbd8d07ed5
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2025-06-08 20:11:56 -0300

    Add posts endpoint

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message":"System operating successfully."}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "These are your posts"}
