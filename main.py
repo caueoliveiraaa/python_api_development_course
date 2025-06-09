8201d6184cb5c571cde5caf05d9d350a8727732e
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2025-06-08 21:22:58 -0300

    Add post endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 
 app = FastAPI()
@@ -6,9 +7,15 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message":"System operating successfully."}
+    return {"message":"System operating successfully"}
 
 
 @app.get("/posts")
 def get_posts():
     return {"data": "These are your posts"}
+
+
+@app.post("/createposts")
+def get_post(payload: dict = Body):
+    return {"message": "sucessfully created post"}
+    
