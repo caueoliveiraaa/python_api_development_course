6fbda360b3f4245aef23b9d6ee3e9846cfdc627e
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2025-06-14 15:26:11 -0300

    Add pylint improvements

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""" Entry point of the application """
 from fastapi import FastAPI
 from fastapi.params import Body
 
@@ -7,15 +8,18 @@ app = FastAPI()
 
 @app.get("/")
 def root():
+    """ Root endpoint """
     return {"message":"System operating successfully"}
 
 
 @app.get("/posts")
 def get_posts():
+    """ Fetch posts """
     return {"data": "These are your posts"}
 
 
 @app.post("/createposts")
 def get_post(payload: dict = Body):
+    """ Generate new posts """
+    print(payload)
     return {"message": "sucessfully created post"}
-    
